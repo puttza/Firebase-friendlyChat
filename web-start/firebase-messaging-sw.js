c37e81cac0738266d1295503d85aa8a1e904fd2d
puttza <prapatsorn416@gmail.com>
2018-09-04 15:46:00 +0700

    update Firebase version 4.2.1 and change code style in main.js

--- web-start/firebase-messaging-sw.js
+++ web-start/firebase-messaging-sw.js
@@ -1,21 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-messaging.js');
+importScripts('/__/firebase/5.1.0/firebase-app.js');
+importScripts('/__/firebase/5.1.0/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
 
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
-firebase.initializeApp({
-  messagingSenderId: '1099469579269'
-});
-var messaging = firebase.messaging();
-
-//messaging.setBackgroundMessageHandler(function(payload) {
-//    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//
-//    const notificationTitle = 'Background Message Title';
-//    const notificationOptions = {
-//        body: 'Background Message body.',
-//        icon: '/firebase-logo.png'
-//    };
-//
-//    return self.registration.showNotification(notificationTitle, notificationOptions);
-//});
+firebase.messaging();
